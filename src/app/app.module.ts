32fb847309660ae8278d173cd67d3ef030e91648
rangkeswaha <c14180243@john.petra.ac.id>
2021-03-02 19:15:30 +0700

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { DetailComponent } from './detail/detail.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { RouterModule, Routes } from '@angular/router';
+import { GlobalvarService } from './globalvar.service';
+
+const ROUTES : Routes = [
+  {path: "detail", component: DetailComponent},
+  {path: "favorite", component: FavoriteComponent},
+]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(ROUTES) ],
+  declarations: [ AppComponent, HelloComponent, DetailComponent, FavoriteComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [GlobalvarService]
 })
 export class AppModule { }
